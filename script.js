8bace575f0e6bab942d6735a77dc39630717f3d4
Science Worldz <79738479+Zatan16@users.noreply.github.com>
2022-02-07 12:17:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,4 @@ const ctx = canvas.getContext("2d")
 canvas.width = window.innerWidth; canvas.height = window.innerHeight;
 const width = canvas.width, height = canvas.height;
 
-shape.draw.polygon(ctx, [
-	{x: 100, y: 100},
-	{x: 200, y: 100},
-	{x: 200, y: 300},
-	{x: 150, y: 250},
-], "red", "green", 3);
+// This is basic structure for drawing on HTML5 canvas and this will be used by all the programmers to draw shapes on HTML5 canvas.
